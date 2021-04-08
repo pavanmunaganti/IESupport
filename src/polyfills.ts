da9447e1f8f5bb218758d8f8d72da6c5892cb493
Pavan Munaganti <munaganti.pavan@gmail.com>
2021-04-08 16:20:20 +0530

    S commit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,7 +17,7 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
- import 'core-js';
+import 'core-js';
 /**
  * IE11 requires the following for NgClass support on SVG elements
  */
